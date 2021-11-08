142afd9c9192ee4576c35a7c7aa539767277438b
Marco Polo Jácome Toss <jacometoss@gmail.com>
2021-11-07 23:55:07 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ exclude_patterns = []
 show_authors = True
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+#pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bizstyle'
 #html_theme = 'sphinx_rtd_theme''
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
