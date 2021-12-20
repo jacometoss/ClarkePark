28d0a71459ac773b314c17378a3e864dfe16c995
Marco Polo Jácome Toss <jacometoss@gmail.com>
2021-12-20 15:15:12 -0600

    0.1.6

--- docs/conf.py
+++ docs/conf.py
@@ -218,7 +218,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-_stdauthor = r''
+#_stdauthor = r''
 
 documents = [
   ('index', 'clarkepark.tex', u' Transformación de Clarke & Park', _stdauthor, 'manual'),
